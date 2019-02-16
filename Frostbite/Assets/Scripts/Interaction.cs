5864e386f4f327358bbac1e19226605ab29f54aa
SusanSuY <yuens2@mcmaster.ca>
2019-02-16 17:29:34 -0500

    Imported assets. Created initial game scene. Setup player controller and skybox. Wrote Stats and Interaction scripts.

---
+++ Frostbite/Assets/Scripts/Interaction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Character controller script to allow character to interact with items.
+/// </summary>
+public class Interaction : MonoBehaviour
+{
+    private const float maxDistance = 1.5f;     // max distance the player can be from an item in order to interact with it
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Player presses key F, attempting to interact with items
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            // Generate raycast to detect objects in front of player
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+            {
+                // Check if object is within interactable distance
+                if (hit.distance < maxDistance)
+                {
+                    // Perform item interaction here!
+                    Debug.Log("Item interacted.");
+                }
+            }
+        }
+    }
+}
---
+++ Frostbite/Assets/Scripts/Stats.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  This class contains the stat values of the character.
+/// </summary>
+public class Stats : MonoBehaviour
+{
+    private int health;         // hp of character, from 0 to 100
+    private int temperature;    // temperature of character, ranging from 0 to 35
+
+    // Use this for initialization
+    void Start()
+    {
+        // Set stats to starting values
+        health = 100;
+        temperature = 35;
+    }
+
+    /// <summary>
+    ///  Returns the speed that the character should be moving at based on their current temperature.
+    /// </summary>
+    /// <returns>Speed of the character.</returns>
+    public int GetSpeed()
+    {
+        // Calculate character speed based on temperature
+        int speed = temperature / 2;
+        return speed;
+    }
+
+    /// ----------------------------------------------------
+    /// Getters and setters
+    /// ----------------------------------------------------
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    public void SetHealth(int health)
+    {
+        this.health = health;
+    }
+
+    public int GetTemperature()
+    {
+        return temperature;
+    }
+
+    public void SetTemperature(int temperature)
+    {
+        this.temperature = temperature;
+    }
+}
