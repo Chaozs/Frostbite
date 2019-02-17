22861c14ac1efe196b1ce2bcc3194f59d7419eaa
SusanSuY <yuens2@mcmaster.ca>
2019-02-17 01:27:22 -0500

    Added torch, fire, terrain. Updated skybox.

---
+++ Frostbite/Assets/Scripts/Animation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    public Transform torch;
+    
+    void Update()
+    {
+        /// Walking/idle torch animation
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            // Play running animation if character is moving
+            torch.GetComponent<Animator>().Play("Running");
+        }
+        else
+        {
+            // Play idle animation if character is not moving
+            torch.GetComponent<Animator>().Play("Idle");
+        }
+    }
+}
