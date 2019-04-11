1b0d672d0a92234df8796d68c4b04c24fe2b3639
SusanSuY <yuens2@mcmaster.ca>
2019-04-11 17:50:46 -0400

    Remove health stat

--- Frostbite/Assets/Scripts/Stats.cs
+++ Frostbite/Assets/Scripts/Stats.cs
@@ -8,15 +8,13 @@ using UnityEngine;
 public class Stats : MonoBehaviour
 {
     private Torch torch;    // script attached to the torch
-
-    [SerializeField] private int health;                // hp of character, ranging from 0 to 100
+    
     [SerializeField] private int temperature;           // temperature of character, ranging from 0 to 35; determines walking speed
     [SerializeField] private GameObject openBook;       // book on dead body giving player hints on what to do
     [SerializeField] private GameObject[] torchStuff;
-    private bool isLosingHealthAndTemp;                 // indicates if coroutine LoseHealthAndTempOverTime() is running
-    private bool isGainingHealthAndTemp;                // indicates if coroutine GainHealthAndTempOverTime() is running
+    private bool isLosingWarmth;
+    private bool isGainingWarmth;
     public bool inMonsterRange;
-    private bool isTakingDamageFromMonster;             // indicates if coroutine loseHealthEnemy() is running
     private int pagesLeft;                              // how many pages left to burn
     private PlayerController playerController;
     private bool isReading;
@@ -29,11 +27,9 @@ public class Stats : MonoBehaviour
     void Awake()
     {
         // Set stats to starting values
-        health = 100;
         temperature = 35;
-        isLosingHealthAndTemp = false;
-        isGainingHealthAndTemp = false;
-        isTakingDamageFromMonster = false;
+        isLosingWarmth = false;
+        isGainingWarmth = false;
         pagesLeft = 3;
         playerController = gameObject.GetComponent<PlayerController>();
         foreach (GameObject playerItem in torchStuff)
@@ -78,27 +74,27 @@ public class Stats : MonoBehaviour
         if (!torch.IsLit())
         {
             // If torch is not lit, lose HP and temperature over time
-            if (!isLosingHealthAndTemp)
+            if (!isLosingWarmth)
             {
-                StartCoroutine(LoseHealthAndTempOverTime());
+                StartCoroutine(LoseTemperatureOverTime());
             }
 
-            if (isGainingHealthAndTemp)
+            if (isGainingWarmth)
             {
-                isGainingHealthAndTemp = false;
+                isGainingWarmth = false;
             }
         }
         else if (torch.IsLit())
         {
             // If torch is lit, gain HP and temperature over time
-            if (!isGainingHealthAndTemp)
+            if (!isGainingWarmth)
             {
-                StartCoroutine(GainHealthAndTempOverTime());
+                StartCoroutine(GainTemperatureOverTime());
             }
 
-            if (isLosingHealthAndTemp)
+            if (isLosingWarmth)
             {
-                isLosingHealthAndTemp = false;
+                isLosingWarmth = false;
             }
         }
     }
@@ -186,26 +182,15 @@ public class Stats : MonoBehaviour
     }
 
     /// <summary>
-    /// Coroutine which loses health and temperature over time.
-    /// If nothing is lit, health is lost at a rate of 3 health per 2 seconds (subject to change), or per 4 seconds if lighter is on.
+    /// Coroutine which loses temperature over time.
     /// If nothing is lit, temperature is lost at a rate of 1 degree per 2 seconds (subject to change), or per 4 seconds if lighter is on.
     /// Called when the torch is not lit, corresponding to the character freezing.
     /// </summary>
-    private IEnumerator LoseHealthAndTempOverTime()
+    private IEnumerator LoseTemperatureOverTime()
     {
-        isLosingHealthAndTemp = true;
-        while (isLosingHealthAndTemp)
+        isLosingWarmth = true;
+        while (isLosingWarmth)
         {
-            // Lose health
-            if (health >= 3)
-            {
-                health -= 3;
-            }
-            else if (health > 0)
-            {
-                health = 0;
-            }
-
             // Lose temperature
             if (temperature > 0)
             {
@@ -215,42 +200,31 @@ public class Stats : MonoBehaviour
             if (GameObject.FindGameObjectWithTag("Lighter") != null)
             {
                 // If lighter is equipped, lose HP/temp slower
-                yield return new WaitForSeconds(4);
+                yield return new WaitForSeconds(2);
             }
             else if (IsLookingAtSnowman())
             {
                 // Lose HP/temp faster if looking at snowman
-                yield return new WaitForSeconds(1);
+                yield return new WaitForSeconds(0.5f);
             }
             else
             {
                 // Default speed to lose HP/temp
-                yield return new WaitForSeconds(2);
+                yield return new WaitForSeconds(1);
             }
         }
     }
 
     /// <summary>
-    /// Coroutine which gains health and temperature over time.
-    /// Health is gained at a rate of 2 health per 1 seconds (subject to change).
+    /// Coroutine which gains temperature over time.
     /// Temperature is gained at a rate of 2 degrees per 1 seconds (subject to change).
     /// Called when the torch is lit, corresponding to the character warming up.
     /// </summary>
-    private IEnumerator GainHealthAndTempOverTime()
+    private IEnumerator GainTemperatureOverTime()
     {
-        isGainingHealthAndTemp = true;
-        while (isGainingHealthAndTemp)
+        isGainingWarmth = true;
+        while (isGainingWarmth)
         {
-            // Gain hp
-            if (health <= 97)
-            {
-                health += 2;
-            }
-            else if (health < 100)
-            {
-                health = 100;
-            }
-
             // Gain temperature
             if (temperature < 35)
             {
@@ -258,61 +232,27 @@ public class Stats : MonoBehaviour
             }
             yield return new WaitForSeconds(1);
         }
-        isGainingHealthAndTemp = false;
-    }
-
-    /// <summary>
-    /// Lose health from monster
-    /// </summary>
-    private IEnumerator loseHealthEnemy()
-    {
-        isTakingDamageFromMonster = true;
-
-        while (isTakingDamageFromMonster)
-        {
-            // lose HP
-            if (health > 0)
-            {
-                health = health - 30;
-            }
-            else if (health < 0)
-            {
-                health = 0;
-            }
-            print(health);
-            yield return new WaitForSeconds(3);
-        }
-
-        isTakingDamageFromMonster = false;
+        isGainingWarmth = false;
     }
 
     public void KillPlayer()
     {
-        health = 0;
+        temperature = 0;
     }
 
     /// <summary>
-    /// Current alive/death status of character, based on health.
-    /// If health is 0, character is deemed dead. If health > 0, character is alive.
+    /// Current alive/death status of character, based on temperature.
+    /// If temperature is 0, character is deemed dead. If temperature > 0, character is alive.
     /// </summary>
-    /// <returns>true is player is dead, false if player is alive.</returns>
+    /// <returns>True if player is dead, false if player is alive.</returns>
     public bool IsDead()
     {
-        return (health <= 0);
+        return (temperature <= 0);
     }
 
     /// ----------------------------------------------------
     /// Getters and setters
     /// ----------------------------------------------------
-    public int GetHealth()
-    {
-        return health;
-    }
-
-    public void SetHealth(int health)
-    {
-        this.health = health;
-    }
 
     public int GetTemperature()
     {
